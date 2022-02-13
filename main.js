c68e391c40312525970363ee3ce1e87ed128502c
Gardenwm <71957742+Gardenwm@users.noreply.github.com>
2022-02-12 23:00:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-function setup()
-{
-    video = createCapture(VIDEO);
-    video.size(600, 600);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas = createCanvas(700, 700);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('poses', gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded()
-{
-    console.log('posenet initialized');
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
 }
 
-function gotPoses(results)
-{
-    if(results.length>0)
-    {
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
+}
+
+function draw() {
+    background('#fff');
+    stroke('#000')
+    textSize(difference);
+    fill('#000');
+    text('anhiti', 50, 400);
 }
